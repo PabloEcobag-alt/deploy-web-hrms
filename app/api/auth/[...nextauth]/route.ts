9d0433fc1aa2631743f73c5e11b9371edc2b3eae
Papa P <papap@Tetets-MacBook-Air.local>
2026-09-25 07:56:33 +0800

    Restore NextAuth API route handlers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,2 @@
-/*
 import { handlers } from "@/auth";
 export const { GET, POST } = handlers;
-*/
-export default function Mock() {}
-export const GET = function MockGet() {};
-export const POST = function MockPost() {};
